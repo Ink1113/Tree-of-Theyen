4795023620ed52c87f7b3fab83892fa43fd5be00
Ink1113 <166874603+Ink1113@users.noreply.github.com>
2024-04-22 23:41:23 -0400

    Update charadex.js

--- styles/charadex.js
+++ styles/charadex.js
@@ -2,7 +2,7 @@
 /* URLs
 ======================================================================= */
 let url = new URL(window.location.href);
-let baseURL = window.location.origin + '/' + window.location.pathname;
+let baseURL = window.location.origin + window.location.pathname;
 let folderURL = window.location.origin + '/' + window.location.pathname.replace(/\/[^\/]+$/, "");
 let urlParams = new URLSearchParams(window.location.search);
 
@@ -719,7 +719,7 @@ const frontPage = (options) => {
 
                 // Add cardlink
                 let cardKey = Object.keys(selectDesigns[0])[0];
-                for (var i in selectDesigns) { selectDesigns[i].cardlink = folderURL + "masterlist.html?" + cardKey + "=" + selectDesigns[i][cardKey]; }
+                for (var i in selectDesigns) { selectDesigns[i].cardlink = folderURL + '/' + "masterlist.html?" + cardKey + "=" + selectDesigns[i][cardKey]; }
 
                 // Nyoom
                 let galleryOptions = {
