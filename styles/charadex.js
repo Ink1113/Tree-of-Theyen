77deca8a96304d95b2668704f0cac3b5add739d5
Ink1113 <166874603+Ink1113@users.noreply.github.com>
2024-04-26 17:52:50 -0400

    Update charadex.js

--- styles/charadex.js
+++ styles/charadex.js
@@ -56,7 +56,7 @@ const scrubData = (sheetData) => {
 let optionSorter = (options) => {
 
     // Clean up the sheetID - in case they used a link instead
-    let scrubbedSheetId = sheetID ? sheetID.includes('/d/') ? sheetID.split('/d/')[1].split('/edit')[0] : sheetID : "1g4euNx8sMxZFHl7VWrrdKUxk0iaP48B8rOX5_Sge7rE";
+    let scrubbedSheetId = sheetID ? sheetID.includes('/d/') ? sheetID.split('/d/')[1].split('/edit')[0] : sheetID : "1l_F95Zhyj5OPQ0zs-54pqacO6bVDiH4rlh16VhPNFUc";
 
     // Call all options, make defaults of our own
     let userOptions = options;
@@ -233,6 +233,7 @@ let charadexFilterSelect = (info, arr, key) => {
 };
 
 
+
 /* ================================================================ */
 /* Faux Folder Function
 /* ================================================================ */
@@ -272,6 +273,7 @@ let fauxFolderButtons = (array, fauxFolder, params = urlParams) => {
     }
 
 };
+
 
 
 
